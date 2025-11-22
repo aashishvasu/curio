4b7259b249cfa5eafb1ddda79c283e562744b316
Aashish Vasudevan <aashishvasu@gmail.com>
2025-11-22 23:23:02 +0530

    feat: Added platform abstraction classes for window creation

---
+++ curiolib/Public/Core/Platform/CuWindowSDL.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "CuWindow.h"
+
+struct SDL_Window;
+namespace CuCore
+{
+	class CuWindowSDL final : public CuWindow
+	{
+	public:
+		CuWindowSDL() = default;
+		~CuWindowSDL() override;
+
+		// CuWindow Interface
+		void Create(const char* Title, int Width, int Height) override;
+		void Close() override;
+		bool ShouldClose() const override;
+		void PollEvents() override;
+		// CuWindow Interface
+	private:
+		// TODO: Replace with custom allocator
+		SDL_Window* Window = nullptr;
+
+		bool CanClose = false;
+	};
+}
